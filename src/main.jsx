e4867bc9f4f47ebf26fa1bb7bfa0b434ae0a9dc8
rennoesche <mad@github.com>
2024-06-01 02:06:42 +0700

    quickfix:   src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App.jsx'
 import './index.css'
-import { ThemeProvider } from "@material-tailwind/react";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider>
     <Router>
       <App />
     </Router>
-    </ThemeProvider>
   </React.StrictMode>,
 )
