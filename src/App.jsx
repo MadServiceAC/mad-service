b3a946e425e2ce1d8d290677b88b19fca2e736fa
rennoesche <mad@github.com>
2024-06-01 02:17:09 +0700

    modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Home from "./pages/home/Home"
 import Login from "./pages/login/Login"
 import Register from "./pages/register/Register"
-import { Route, Routes, Link } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Service from "./pages/user/pages/Service";
 
 function App() {
