fde5b9a915b8e32fd2033065913d5b770e9caa83
rennoesche <mad@github.com>
2024-06-12 11:14:27 +0700

    modified:   backend/utils/utils.js

--- backend/utils/utils.js
+++ backend/utils/utils.js
@@ -1,41 +1,7 @@
 async function createUsersTable() {
-    const createTableQuery = `
-      CREATE TABLE IF NOT EXISTS users (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        fullname VARCHAR(255) NOT NULL,
-        email VARCHAR(255) UNIQUE NOT NULL,
-        password VARCHAR(255) NOT NULL,
-        role ENUM('user', 'seller', 'teknisi') NOT NULL DEFAULT 'user'
-      )
-    `;
-    try {
-      await db.promise().query(createTableQuery);
-      console.log("Tabel 'users' berhasil dibuat atau sudah ada.");
-    } catch (error) {
-      console.error("Error saat membuat tabel 'users':", error);
-      throw error;
-    }
-  }
+}
 
 async function createItemsTable() {
-    const createTableQuery = `
-      CREATE TABLE IF NOT EXISTS items (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        name VARCHAR(255) NOT NULL,
-        sku VARCHAR(50) UNIQUE NOT NULL,
-        price DECIMAL(10, 2) NOT NULL,
-        description TEXT,
-        quantity INT NOT NULL,
-        image VARCHAR(255),
-      )
-    `; 
-  
-    try {
-      await db.promise().query(createTableQuery);
-      console.log("Tabel 'items' berhasil dibuat atau sudah ada.");
-    } catch (error) {
-      console.error("Error saat membuat tabel 'items':", error);
-      throw error; 
-    }
-  }
-  
+}
+
+export { createUsersTable, createItemsTable }; 
